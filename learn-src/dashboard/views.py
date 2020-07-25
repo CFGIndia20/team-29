02f7a2a29e7ff102f126d35ab52d088e005a7693
Karan Idnani <karanidnani10@gmail.com>
2020-07-25 19:13:13 +0530

    dashboard views

--- learn-src/dashboard/views.py
+++ learn-src/dashboard/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+#from .models import registration_details
 # Create your views here.
 
 
 def home(request):
     return render(request, 'home/home.html')
+
+def studentregistration(request):
+    if request.method == 'POST':
+        post=Post()
+        post.firstname = request.POST.get('firstname')
+        post.lname = request.POST.get('lname')
+        post.email = request.POST.get('email')
+        post.pno = request.POST.get('pno')
+        post.save()       
+        return render(request, 'posts/create.html')  
+    
+    else:
+        return render(request,'posts/create.html')
