84ad3da96bb0b756937bd41978001e2a69b17de0
Anshul Chaudhary <anshulchaudhary.218@gmail.com>
2020-07-25 19:18:41 +0530

    company details and data storage views done
    views, urls, form, models copmleted

--- employee-src/accounts/urls.py
+++ employee-src/accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/login', views.login , name = 'login'),
+    path('login', views.login , name = 'login'),
     
 ]
--- employee-src/dashboard/models.py
+++ employee-src/dashboard/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class company_details(models.Model):
+    company_name = models.CharField(max_length=255)
+    contact_email = models.EmailField(max_length=255)
+    job_profile =  models.CharField(max_length=255)
+    job_description = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.company_name
+
+class skills_required(models.Model):
+    company_id = models.ForeignKey(company_details, on_delete=models.CASCADE)
+    skills_required = models.CharField(max_length=255)
