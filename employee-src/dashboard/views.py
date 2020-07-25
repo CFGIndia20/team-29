ebd80a54030a248f66ad5f46c3c4429a2d4b9906
Dont Know <dingtheas1973@gmail.com>
2020-07-25 19:42:19 +0530

    Added Company Card and Navbar

--- employee-src/dashboard/views.py
+++ employee-src/dashboard/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 
+companies = [
+    {
+    "name": "JOOJOO",
+    "desc": "MOOOJOO",
+    "email": "mojoJOJO",
+    }
+]
+user = {
+    "name": "Anshul",
+    "email": "anshul@sss.com",
+}
 def home(request):
-    return render(request, 'base/base.html')
+    return render(request, 'home/home.html', {'companies': companies, 'user': user })
 
 def form(request):
     return render(request, 'company_form/form.html')
