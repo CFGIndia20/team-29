ad1e11ee291f902709e139bbf1dbef736f8600ce
Karan Idnani <karanidnani10@gmail.com>
2020-07-26 03:17:15 +0530

    static images loaded, dashboard integrated

--- learn-src/dashboard/views.py
+++ learn-src/dashboard/views.py
@@ -1,7 +1,5 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-# Create your views here.
 
-
-def home(request):
-    return render(request, 'home/home.html')
+def student_teacher(request):
+    return render(request, 'dashboard/student-teacher.html')
